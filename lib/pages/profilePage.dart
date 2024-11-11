866effa4d44c5f3e79c157df452cd035e810baec
Demonslayer6408 <69177286+Demonslayer6408@users.noreply.github.com>
2024-11-11 22:18:28 +0800

    PROFILEPAGE DSFSDFDSFDSF

--- lib/pages/profilePage.dart
+++ lib/pages/profilePage.dart
@@ -25,19 +25,21 @@ class ProfilePage extends StatelessWidget {
     }
 
     return BlocProvider(
-      create: (context) => UserBloc(DatabaseHelper())..add(FetchUser(userId: userId!)), // Fetch user data on page load
+      create: (context) => UserBloc(DatabaseHelper())..add(FetchUser(userId: userId!)),
       child: Scaffold(
+        backgroundColor: Colors.white, 
         appBar: AppBar(
-          title: Text('Profile'),
+          title: null,
+          automaticallyImplyLeading: false,
         ),
         body: BlocBuilder<UserBloc, UserState>(
           builder: (context, state) {
             if (state is Loading) {
-              return Center(child: CircularProgressIndicator()); // Loading state
+              return Center(child: CircularProgressIndicator());
             } else if (state is Error) {
-              return Center(child: Text('Error: ${state.message}')); // Error state
+              return Center(child: Text('Error: ${state.message}'));
             } else if (state is UserFetched) {
-              final user = state.user; // Your fetched user data
+              final user = state.user;
 
               if (user == null) {
                 return Center(child: Text('User not found'));
@@ -48,76 +50,157 @@ class ProfilePage extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    // User Image with Loading Indicator
-                    user.imageUrl != null
-                        ? CircleAvatar(
-                            radius: 50,
-                            backgroundColor: Colors.grey.shade300,
-                            child: ClipOval(
-                              child: Image.network(
-                                user.imageUrl!,
-                                width: 100,
-                                height: 100,
-                                fit: BoxFit.cover,
-                                loadingBuilder: (context, child, loadingProgress) {
-                                  if (loadingProgress == null) {
-                                    // Image has finished loading
-                                    return child;
-                                  } else {
-                                    // Show a CircularProgressIndicator while loading
-                                    return Center(
-                                      child: CircularProgressIndicator(),
-                                    );
-                                  }
-                                },
-                                errorBuilder: (context, error, stackTrace) {
-                                  // Show a fallback icon if the image fails to load
-                                  return Icon(Icons.person, size: 50, color: Colors.white);
-                                },
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white, // Background color for the image container
+                              shape: BoxShape.circle,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 4,
+                                  blurRadius: 8,
+                                  offset: Offset(0, 4), // Shadow position
+                                ),
+                              ],
+                            ),
+                            child: CircleAvatar(
+                              radius: 100,
+                              backgroundColor: Colors.grey.shade300,
+                              child: ClipOval(
+                                child: user.imageUrl != null
+                                    ? Image.network(
+                                        user.imageUrl!,
+                                        width: 200,
+                                        height: 200,
+                                        fit: BoxFit.cover,
+                                        loadingBuilder: (context, child, loadingProgress) {
+                                          if (loadingProgress == null) {
+                                            return child;
+                                          } else {
+                                            return Center(child: CircularProgressIndicator());
+                                          }
+                                        },
+                                        errorBuilder: (context, error, stackTrace) {
+                                          return Icon(Icons.person, size: 200, color: Colors.white);
+                                        },
+                                      )
+                                    : Icon(Icons.person, size: 200, color: Colors.white),
                               ),
                             ),
-                          )
-                        : CircleAvatar(
-                            radius: 50,
-                            backgroundColor: Colors.grey,
-                            child: Icon(Icons.person, size: 50, color: Colors.white),
                           ),
+                          SizedBox(height: 16), 
+                          Text(
+                            user.fullName ?? 'Unknown', 
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                     SizedBox(height: 16),
 
-                    // User Information
-                    Text(
-                      'Name: ${user.fullName ?? 'Unknown'}',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Email: ${user.email ?? 'Not available'}',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Phone: ${user.phoneNo ?? 'Not available'}',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Position: ${user.jobPosition ?? 'Not available'}',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'School: ${user.school ?? 'Not available'}',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Birthday: ${user.birthdate != null ? formatDate(user.birthdate!) : 'Not available'}',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Address: ${user.address ?? 'Not available'}',
-                      style: TextStyle(fontSize: 16),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10), 
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            spreadRadius: 3,
+                            blurRadius: 6,
+                            offset: Offset(0, 3), 
+                          ),
+                        ],
+                      ),
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.person, size: 24, color: Colors.black),
+                              SizedBox(width: 8),
+                              Text(
+                                user.fullName ?? 'Unknown',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8),
+                          Row(
+                            children: [
+                              Icon(Icons.email, size: 24, color: Colors.black),
+                              SizedBox(width: 8),
+                              Text(
+                                user.email ?? 'Not available',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8),
+                          Row(
+                            children: [
+                              Icon(Icons.phone, size: 24, color: Colors.black),
+                              SizedBox(width: 8),
+                              Text(
+                                user.phoneNo ?? 'Not available',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8),
+                          Row(
+                            children: [
+                              Icon(Icons.work, size: 24, color: Colors.black),
+                              SizedBox(width: 8),
+                              Text(
+                                user.jobPosition ?? 'Not available',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8),
+                          Row(
+                            children: [
+                              Icon(Icons.school, size: 24, color: Colors.black),
+                              SizedBox(width: 8),
+                              Text(
+                                user.school ?? 'Not available',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8),
+                          Row(
+                            children: [
+                              Icon(Icons.cake, size: 24, color: Colors.black),
+                              SizedBox(width: 8),
+                              Text(
+                                user.birthdate != null ? formatDate(user.birthdate!) : 'Not available',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8),
+                          Row(
+                            children: [
+                              Icon(Icons.home, size: 24, color: Colors.black),
+                              SizedBox(width: 8),
+                              Text(
+                                user.address ?? 'Not available',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
